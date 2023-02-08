d4ad9320c73e8d88f29868bedb56ba08aae1892e
Zhu Zijun <2961163526@qq.com>
2023-02-08 23:13:30 +0800

    新增用于微服务异常注册并自动abort的grpc_method_error_handler装饰器

--- python_package/_321CQU/tools/__init__.py
+++ python_package/_321CQU/tools/__init__.py
@@ -2,6 +2,7 @@ from .ConfigHandler import *
 from .Singleton import *
 from .gRPCManager import *
 from .protobufBridge import *
+from .gRPCMethodErrorHandler import *
 
 __all__ = ['ConfigHandler', 'Singleton', 'singleton', 'gRPCManager', 'MockGRPCManager', 'ServiceEnum',
-           'model2protobuf', 'model_list2protobuf']
+           'model2protobuf', 'model_list2protobuf', 'grpc_method_error_handler']
