587c623eda49d350ab85e32f61e0fd3dd9725b05
ConstantineQAQ <357208746@qq.com>
2024-03-14 14:48:52 +0800

    refactor(Service.py): 移除Service.py中获取stub和service_name的大量if else操作，改为handler适配器模式处理

--- python_package/_321CQU/tools/gRPCManager.py
+++ python_package/_321CQU/tools/gRPCManager.py
@@ -9,10 +9,13 @@ from .Singleton import Singleton
 __all__ = ['gRPCManager', 'MockGRPCManager']
 
 from ..service import ServiceEnum
+from ..service_handler import ServiceHandler
 
 
 class gRPCManager(metaclass=Singleton):
     def __init__(self, handler: ConfigHandler = _CONFIG_HANDLER):
+        # 初始化时创建每个ServiceHandler子类的实例
+        self.service_instances = [cls() for cls in ServiceHandler.__subclasses__()]
         all_options = handler.get_options('ServiceSetting')
 
         service_hosts = list(filter(lambda x: x.endswith('_service_host'), all_options))
@@ -31,22 +34,26 @@ class gRPCManager(metaclass=Singleton):
             })
 
     def get_service_config(self, service: ServiceEnum) -> Tuple[str, str]:
-        return (self._service_host[service.service_name + "_service_host"],
-                self._service_ports[service.service_name + "_service_port"])
+        for instance in self.service_instances:
+            if instance.support(name=service):
+                return (self._service_host[instance.get_service_name + "_service_host"],
+                        self._service_ports[instance.get_service_name + "_service_port"])
 
     @asynccontextmanager
     async def get_stub(self, service: ServiceEnum):
         if service.is_http_service:
             raise RuntimeError("该服务不是gRPC服务")
-
-        target = service._get_stub_class()
-
-        if target is not None:
-            host = self._service_host[service.service_name + "_service_host"]
-            port = self._service_ports[service.service_name + "_service_port"]
-            target_url = host + ":" + port
-            async with insecure_channel(target_url) as channel:
-                yield target(channel)
+        for instance in self.service_instances:
+            if instance.support(name=service):
+                target = instance.get_stub_class()
+                if target is not None:
+                    host = self._service_host[instance.get_service_name + "_service_host"]
+                    port = self._service_ports[instance.get_service_name + "_service_port"]
+                    target_url = host + ":" + str(port)
+                    async with insecure_channel(target_url) as channel:
+                        yield target(channel)
+            else:
+                raise RuntimeError("未提供对应服务Stub")
 
 
 class MockGRPCManager(gRPCManager):
